040464b034e0d4fbe582e84855d15c1d55854178
PurgetheToast <alibob.osbat@googlemail.com>
2017-08-03 10:08:15 +0100

    this is a test commit

--- app.js
+++ app.js
@@ -12,12 +12,50 @@ app.set('views', __dirname + '/views' );
 */
 
 app.get('/', function (req,res) {
-  res.render('indexs', {title: 'fjh67'});
+  res.render('chat', {title: 'chat'});
 });
 
 app.use(express.static('static'));
 
-app.listen(3000, function (){
+var webserver = app.listen(3000, function (){
     console.log('Example app listening on port 3000!')
 });
 
+var WebSocketServer = require('ws').Server, config = {
+    host: "localhost",
+    port: 8080,   
+}, wss = new WebSocketServer(config, function(){
+    console.log('Web Socket Server created and on port 8080');
+});
+
+var n = 1;
+
+var sockets = {};
+
+wss.on('connection', function(ws){
+    
+    var randomGen = new Date().getTime();
+    
+    sockets[randomGen] = ws;
+    
+    console.log('Connected');
+    
+    ws.send('SERVER: Welcome');
+    /*setInterval(function (){
+          n += 1;
+        ws.send('A MESSAGE TO YOU RUDY TIMES ' + n);
+    }, 3000)*/
+  
+
+    ws.on('message', function(message){
+        console.log('message: %s', message);
+        for(var i in sockets){
+            sockets[i].send(message);
+            }
+        });
+    
+    ws.on('close', function(){
+        console.log('connection stopped');
+        delete sockets[randomGen];
+        });
+});
